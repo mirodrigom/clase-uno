e44ea8c9df2c1547bd6dc4938674c4beff5245dd
Rodrigo Martinez <mirodrigom@gmail.com>
2018-08-23 21:27:07 -0300

    Primer listener

--- app/src/main/java/com/example/alumno/clase_uno/MainActivity.java
+++ app/src/main/java/com/example/alumno/clase_uno/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.alumno.clase_uno;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt1 = (TextView) findViewById(R.id.txt1);
+        EditText txe1 = (EditText) findViewById(R.id.txe1);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+
+        txe1.setHint("ver");
+        txt1.setText("--------");
+        btn1.setText("Mostrar!");
+        Escuchador escuchador = new Escuchador();
+        btn1.setOnClickListener(escuchador);
     }
 }
