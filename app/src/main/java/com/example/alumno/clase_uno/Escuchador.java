850452c0c05504fbb6e9b94f4a889ce8e8a3d5f2
Rodrigo Martinez <mirodrigom@gmail.com>
2018-08-23 22:21:54 -0300

    Listener seteado

---
+++ app/src/main/java/com/example/alumno/clase_uno/Escuchador.java
@@ -0,0 +1,28 @@
+package com.example.alumno.clase_uno;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by alumno on 23/08/2018.
+ */
+
+public class Escuchador implements View.OnClickListener {
+
+    TextView txt1;
+    EditText edt1;
+
+    public Escuchador(TextView txt1, EditText edt1)
+    {
+        this.txt1 = txt1;
+        this.edt1 = edt1;
+    }
+
+    @Override
+    public void onClick(View v) {
+        txt1.setText(edt1.getText().toString());
+    }
+}
